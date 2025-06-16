ef3b5a2fc6034931e90ccac71361e45d23942158
Dominique Lippelt <dlippelt@f1r2s7.codam.nl>
2025-06-16 17:41:34 +0200

    Read .cub file into a char**

--- src/validation/ft_validate_file_content.c
+++ src/validation/ft_validate_file_content.c
@@ -6,7 +6,7 @@
 /*   By: dlippelt <dlippelt@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/16 15:58:08 by dlippelt          #+#    #+#             */
-/*   Updated: 2025/06/16 16:43:32 by dlippelt         ###   ########.fr       */
+/*   Updated: 2025/06/16 17:36:59 by dlippelt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,9 +38,39 @@ static ssize_t	ft_get_num_lines(int fd, t_map *m)
 	return (1);
 }
 
+static int	ft_get_map_file_content(int fd, t_map *m)
+{
+	size_t	line;
+
+	m->map = malloc(sizeof(char *) * (m->nlines + 1));
+	if (!m->map)
+		return (ft_error(EMALLOC));
+	line = 0;
+	while (line <= m->nlines)
+		m->map[line++] = NULL;
+	line = 0;
+	while (line < m->nlines)
+	{
+		m->map[line] = get_next_line(fd);
+		if (!m->map[line])
+			return (ft_error(EMALLOC));
+		// printf("%s", m->map[line]);
+		line++;
+	}
+	return (1);
+}
+
 int	ft_validate_file_content(int fd, t_map *m)
 {
 	if (!ft_get_num_lines(fd, m))
 		return (0);
+	if (!ft_open(m->map_path, &fd))
+		return (0);
+	if (!ft_get_map_file_content(fd, m))
+	{
+		close(fd);
+		return (0);
+	}
+	close(fd);
 	return (1);
 }
