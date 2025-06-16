f402adbe3ac1d40538e86a7a591f3e370c568eeb
Dominique Lippelt <dlippelt@f1r6s8.codam.nl>
2025-06-16 14:17:18 +0200

    Some basic headers

---
+++ inc/cub3D.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3D.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dlippelt <dlippelt@student.codam.nl>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/16 13:50:47 by dlippelt          #+#    #+#             */
+/*   Updated: 2025/06/16 14:12:14 by dlippelt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef cub3D_H
+# define cub3D_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include "mlx.h"
+# include "enums.h"
+# include "utils.h"
+
+
+
+#endif
