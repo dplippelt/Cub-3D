8c6c64083335bf1eee190d5acbf152ac7d9ca9a0
Dominique Lippelt <dlippelt@f1r2s7.codam.nl>
2025-06-16 17:40:10 +0200

    Implement ft_open() function

--- inc/utils.h
+++ inc/utils.h
@@ -6,7 +6,7 @@
 /*   By: dlippelt <dlippelt@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/16 14:02:38 by dlippelt          #+#    #+#             */
-/*   Updated: 2025/06/16 15:26:29 by dlippelt         ###   ########.fr       */
+/*   Updated: 2025/06/16 17:12:48 by dlippelt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,15 @@
 # include <unistd.h>
 # include <string.h>
 # include <errno.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include "enums.h"
 
 int		ft_error(int e);
 ssize_t	ft_strlen(const char *s);
 int		ft_putstr(const char *s, int fd);
 int		ft_strcmp(const char *s1, const char *s2);
+int		ft_open(const char *path, int *fd);
 
 #endif
