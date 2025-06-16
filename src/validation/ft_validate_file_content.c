7d9a7a6a095b30fb8a06b9fbbc60baa4b2ab4a34
Dominique Lippelt <dlippelt@f1r2s7.codam.nl>
2025-06-16 16:44:02 +0200

    Simplify ft_read()

--- src/validation/ft_validate_file_content.c
+++ src/validation/ft_validate_file_content.c
@@ -6,15 +6,16 @@
 /*   By: dlippelt <dlippelt@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/16 15:58:08 by dlippelt          #+#    #+#             */
-/*   Updated: 2025/06/16 16:36:13 by dlippelt         ###   ########.fr       */
+/*   Updated: 2025/06/16 16:43:32 by dlippelt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "validation.h"
 
-static int	ft_read(int fd, char *buffer, size_t nbytes, t_map *m)
+static int	ft_read(int fd, t_map *m)
 {
 	static ssize_t	br;
+	static char		buffer[1];
 
 	br = read(fd, buffer, 1);
 	if (br == -1)
@@ -23,14 +24,12 @@ static int	ft_read(int fd, char *buffer, size_t nbytes, t_map *m)
 		return (1);
 	if (*buffer == '\n')
 		m->nlines++;
-	return (ft_read(fd, buffer, nbytes, m));
+	return (ft_read(fd, m));
 }
 
 static ssize_t	ft_get_num_lines(int fd, t_map *m)
 {
-	char	buffer[1];
-
-	if (!ft_read(fd, buffer, 1, m))
+	if (!ft_read(fd, m))
 	{
 		close(fd);
 		return (0);
@@ -43,6 +42,5 @@ int	ft_validate_file_content(int fd, t_map *m)
 {
 	if (!ft_get_num_lines(fd, m))
 		return (0);
-
 	return (1);
 }
