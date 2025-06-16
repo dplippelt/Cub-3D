32bfdbacf553df4c9fecf6bb5d1144c39bbb043c
Dominique Lippelt <dlippelt@f1r2s7.codam.nl>
2025-06-16 15:30:15 +0200

    Rename file

--- src/utils/ft_putstr.c
+++ src/utils/ft_putstr.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_write.c                                         :+:      :+:    :+:   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dlippelt <dlippelt@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/16 13:59:29 by dlippelt          #+#    #+#             */
-/*   Updated: 2025/06/16 14:10:44 by dlippelt         ###   ########.fr       */
+/*   Updated: 2025/06/16 15:26:46 by dlippelt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
 
-int	ft_putstr(char *s, int fd)
+int	ft_putstr(const char *s, int fd)
 {
 	size_t	slen;
 
