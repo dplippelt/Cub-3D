b679c497234b2148cd6ff29616491095ec5cd63d
Dominique Lippelt <dlippelt@f1r2s7.codam.nl>
2025-06-18 16:34:02 +0200

    Add struct for color data and texture data

--- inc/structs.h
+++ inc/structs.h
@@ -6,20 +6,39 @@
 /*   By: dlippelt <dlippelt@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/16 16:21:43 by dlippelt          #+#    #+#             */
-/*   Updated: 2025/06/18 12:49:26 by dlippelt         ###   ########.fr       */
+/*   Updated: 2025/06/18 15:58:19 by dlippelt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef STRUCTS_H
 # define STRUCTS_H
 
-#include <unistd.h>
+# include <unistd.h>
+# include <sys/types.h>
+
+typedef struct s_tex
+{
+	char			*tex_path;
+	int				id;
+	struct s_tex	*next;
+}					t_tex;
+
+typedef struct s_col
+{
+	u_int8_t		r;
+	u_int8_t		g;
+	u_int8_t		b;
+	int				id;
+	struct s_col	*next;
+}					t_col;
 
 typedef struct s_file
 {
 	const char	*file_path;
 	char		**file;
 	size_t		nlines;
+	t_tex		*tex;
+	t_col		*col;
 }				t_file;
 
 #endif
