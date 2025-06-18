2956a8d43854d1723a0e80413bf062484adbc982
Dominique Lippelt <dlippelt@f1r2s7.codam.nl>
2025-06-18 16:30:43 +0200

    Add cleanup functions for tex and col linked lists and better organize cleanup functions

--- inc/cleanup.h
+++ inc/cleanup.h
@@ -6,7 +6,7 @@
 /*   By: dlippelt <dlippelt@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/16 16:56:46 by dlippelt          #+#    #+#             */
-/*   Updated: 2025/06/18 12:57:18 by dlippelt         ###   ########.fr       */
+/*   Updated: 2025/06/18 15:53:57 by dlippelt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,9 @@
 # include <stdlib.h>
 # include "structs.h"
 
-void	ft_cleanup_file(t_file *f);
+void	ft_cleanup(t_file *f);
+void	ft_cleanup_file(char **file);
+void	ft_cleanup_tex(t_tex *curr);
+void	ft_cleanup_col(t_col *curr);
 
 #endif
--- src/cleanup/ft_cleanup.c
+++ src/cleanup/ft_cleanup.c
@@ -6,23 +6,55 @@
 /*   By: dlippelt <dlippelt@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/16 16:56:24 by dlippelt          #+#    #+#             */
-/*   Updated: 2025/06/18 12:50:19 by dlippelt         ###   ########.fr       */
+/*   Updated: 2025/06/18 15:53:40 by dlippelt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cleanup.h"
 
-void	ft_cleanup_file(t_file *f)
+void	ft_cleanup_tex(t_tex *curr)
+{
+	t_tex	*next;
+
+	while (curr)
+	{
+		next = curr->next;
+		free(curr->tex_path);
+		free(curr);
+		curr = next;
+	}
+}
+
+void	ft_cleanup_col(t_col *curr)
+{
+	t_col	*next;
+
+	while (curr)
+	{
+		next = curr->next;
+		free(curr);
+		curr = next;
+	}
+}
+
+void	ft_cleanup_file(char **file)
 {
 	size_t	line;
 
-	if (!f->file)
+	if (!file)
 		return ;
 	line = 0;
-	while (f->file[line])
+	while (file[line])
 	{
-		free(f->file[line]);
+		free(file[line]);
 		line++;
 	}
-	free(f->file);
+	free(file);
+}
+
+void	ft_cleanup(t_file *f)
+{
+	ft_cleanup_tex(f->tex);
+	ft_cleanup_col(f->col);
+	ft_cleanup_file(f->file);
 }
