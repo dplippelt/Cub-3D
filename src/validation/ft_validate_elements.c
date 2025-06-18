2e70a2101abd27c6ee0bac684089f136e0d603f9
Dominique Lippelt <dlippelt@f1r2s7.codam.nl>
2025-06-18 17:11:58 +0200

    Check if texture files are readable

--- src/validation/ft_validate_elements.c
+++ src/validation/ft_validate_elements.c
@@ -6,7 +6,7 @@
 /*   By: dlippelt <dlippelt@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/18 11:29:34 by dlippelt          #+#    #+#             */
-/*   Updated: 2025/06/18 16:08:58 by dlippelt         ###   ########.fr       */
+/*   Updated: 2025/06/18 16:53:11 by dlippelt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,14 +45,8 @@ static int	ft_get_elements(t_file *f)
 		while (f->file[y][x])
 		{
 			if (ft_is_element(&f->file[y][x], &el_id))
-			{
-				#ifdef DEBUG
-					debug_print_element_location(el_id, x, y);
-				#endif
-
 				if (!ft_get_element_info(f, &f->file[y][x], el_id))
 					return (0);
-			}
 			x++;
 		}
 		y++;
@@ -60,21 +54,30 @@ static int	ft_get_elements(t_file *f)
 	return (1);
 }
 
+static int	ft_texture_files_readable(t_tex *tex)
+{
+	int	fd;
+
+	while (tex)
+	{
+		if (!ft_open(tex->tex_path, &fd))
+			return (0);
+		close(fd);
+		tex = tex->next;
+	}
+	return (1);
+}
+
 int	ft_validate_elements(t_file *f)
 {
-	#ifdef DEBUG
-		debug_print_map(f);
-	#endif
-
 	if (!ft_get_elements(f))
 		return (0);
-
 	#ifdef DEBUG
+		debug_print_map(f);
 		debug_print_tex_paths(f->tex);
 		debug_print_rbg_values(f->col);
 	#endif
-
-	
-
+	if (!ft_texture_files_readable(f->tex))
+		return (0);
 	return (1);
 }
