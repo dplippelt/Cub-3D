b8cb71c7ad7bd12566f480dede80a5c91fbe8ad2
Dominique Lippelt <dlippelt@f1r2s7.codam.nl>
2025-06-18 16:33:33 +0200

    Add enums for file elements and color channels, as well as adiditional error codes

--- inc/enums.h
+++ inc/enums.h
@@ -6,7 +6,7 @@
 /*   By: dlippelt <dlippelt@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/16 13:56:56 by dlippelt          #+#    #+#             */
-/*   Updated: 2025/06/16 16:46:52 by dlippelt         ###   ########.fr       */
+/*   Updated: 2025/06/18 15:55:56 by dlippelt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,26 @@ enum e_errors
 	ERRNO = 0,
 	ENUMPARAM = 1,
 	EFILEFORMAT = 2,
-	EMALLOC = 3
+	EMALLOC = 3,
+	ECOLORFORMAT = 999
+};
+
+enum e_elements
+{
+	NOELEMENT = 0,
+	TEXNORTH,
+	TEXEAST,
+	TEXWEST,
+	TEXSOUTH,
+	COLFLOOR,
+	COLCEILING
+};
+
+enum e_colors
+{
+	RED,
+	GREEN,
+	BLUE
 };
 
 #endif
