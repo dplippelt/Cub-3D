e8542c56fcd83d89872b3c96bd1ea5990b53e228
Dominique Lippelt <dlippelt@f1r2s7.codam.nl>
2025-06-18 11:52:45 +0200

    Add debugging code

---
+++ inc/debug.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   debug.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dlippelt <dlippelt@student.codam.nl>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/18 11:32:50 by dlippelt          #+#    #+#             */
+/*   Updated: 2025/06/18 11:41:20 by dlippelt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef DEBUG_H
+# define DEBUG_H
+
+# define DEBUG
+
+# include <stdio.h>
+# include "structs.h"
+
+void	debug_print_map(t_map *m);
+
+#endif
