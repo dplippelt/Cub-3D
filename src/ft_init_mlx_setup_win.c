a0813cedd9cf909f65f4a6550b0c325400bda6c9
Dominique Lippelt <dlippelt@f1r1s6.codam.nl>
2025-07-02 12:09:46 +0200

    Calculate window size - By default use 4:3 dimensions set in the WIN_X_DEFAULT and WIN_Y_DEFAULT macros - If one of these is to large for the current screen calculate a smaller 4:3 ratio that does fit on screen

--- src/ft_init_mlx_setup_win.c
+++ src/ft_init_mlx_setup_win.c
@@ -6,20 +6,41 @@
 /*   By: dlippelt <dlippelt@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/17 17:15:13 by tmitsuya          #+#    #+#             */
-/*   Updated: 2025/06/30 10:48:09 by dlippelt         ###   ########.fr       */
+/*   Updated: 2025/07/02 12:02:09 by dlippelt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
+
+static void	ft_set_win_dimesions(t_cub3d *cub)
+{
+	int	win_x;
+	int	win_y;
+
+	cub->win_x = WIN_X_DEFAULT;
+	cub->win_y = WIN_Y_DEFAULT;
+	mlx_get_screen_size(cub->mlx, &win_x, &win_y);
+	if ((win_y * 19/20) < cub->win_y)
+	{
+		cub->win_y = win_y * 19/20;
+		cub->win_x = cub->win_y * 4/3;
+	}
+	else if (win_x < cub->win_x)
+	{
+		cub->win_x = win_x;
+		cub->win_y = cub->win_x * 3/4;
+	}
+}
 
 int	ft_init_mlx_setup_win(t_cub3d *cub)
 {
 	cub->mlx = mlx_init();
 	if (!cub->mlx)
 		return (ft_error(EMLXINIT));
-	mlx_get_screen_size(cub->mlx, &cub->win_x, &cub->win_y);
-	cub->win_x /= 2;
-	cub->win_y = cub->win_y * 3 / 4;
+	ft_set_win_dimesions(cub);
+	// mlx_get_screen_size(cub->mlx, &cub->win_x, &cub->win_y);
+	// cub->win_x /= 2;
+	// cub->win_y = cub->win_y * 3 / 4;
 	cub->win = mlx_new_window(cub->mlx, cub->win_x, cub->win_y, TITLE);
 	if (!cub->win)
 		return (ft_error(EMLXWIN));
