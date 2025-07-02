f4af34715f56578bbd6dc2ad6206c784a9c29d93
Dominique Lippelt <dlippelt@f1r1s6.codam.nl>
2025-07-02 14:44:39 +0200

    Improve name of struct from t_walls to t_map_val

--- src/validation/ft_validate_wall.c
+++ src/validation/ft_validate_wall.c
@@ -6,64 +6,64 @@
 /*   By: dlippelt <dlippelt@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/16 13:55:19 by dlippelt          #+#    #+#             */
-/*   Updated: 2025/07/02 14:22:57 by dlippelt         ###   ########.fr       */
+/*   Updated: 2025/07/02 14:32:05 by dlippelt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "validation.h"
 
-static int	ft_found_hole(t_walls *w, int y, int x)
+static int	ft_found_hole(t_map_val *map, int y, int x)
 {
-	if (w->map[y][x] == '0')
+	if (map->map[y][x] == '0')
 		return (1);
-	if (w->map[y][x] == 'N')
+	if (map->map[y][x] == 'N')
 		return (1);
-	if (w->map[y][x] == 'E')
+	if (map->map[y][x] == 'E')
 		return (1);
-	if (w->map[y][x] == 'S')
+	if (map->map[y][x] == 'S')
 		return (1);
-	if (w->map[y][x] == 'W')
+	if (map->map[y][x] == 'W')
 		return (1);
-	if (w->map[y][x] == 'C')
+	if (map->map[y][x] == 'C')
 		return (1);
 	return (0);
 }
 
-static int	ft_valid_pos(t_walls *w, int y, int x)
+static int	ft_valid_pos(t_map_val *map, int y, int x)
 {
 	if (y < 0)
 		return (0);
-	if (y >= w->height + 2)
+	if (y >= map->height + 2)
 		return (0);
 	if (x < 0)
 		return (0);
-	if (x >= w->width + 2)
+	if (x >= map->width + 2)
 		return (0);
-	if (w->map[y][x] == '1')
+	if (map->map[y][x] == '1')
 		return (0);
-	if (w->map[y][x] == 'X')
+	if (map->map[y][x] == 'X')
 		return (0);
-	if (ft_found_hole(w, y, x))
+	if (ft_found_hole(map, y, x))
 	{
-		w->valid = 0;
+		map->valid = 0;
 		return (0);
 	}
 	return (1);
 }
 
-static void	ft_flood_fill_rec(t_walls *w, int y, int x)
+static void	ft_flood_fill_rec(t_map_val *map, int y, int x)
 {
-	if (!ft_valid_pos(w, y, x))
+	if (!ft_valid_pos(map, y, x))
 		return ;
-	w->map[y][x] = 'X';
-	ft_flood_fill_rec(w, y + 1, x);
-	ft_flood_fill_rec(w, y - 1, x);
-	ft_flood_fill_rec(w, y, x + 1);
-	ft_flood_fill_rec(w, y, x - 1);
+	map->map[y][x] = 'X';
+	ft_flood_fill_rec(map, y + 1, x);
+	ft_flood_fill_rec(map, y - 1, x);
+	ft_flood_fill_rec(map, y, x + 1);
+	ft_flood_fill_rec(map, y, x - 1);
 }
 
-void	ft_validate_wall(t_walls *w)
+void	ft_validate_wall(t_map_val *map)
 {
-	w->valid = 1;
-	ft_flood_fill_rec(w, 0, 0);
+	map->valid = 1;
+	ft_flood_fill_rec(map, 0, 0);
 }
