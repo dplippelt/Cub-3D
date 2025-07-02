8008532fff9b09f9be75ebb98091fda4d1dd5bc1
Dominique Lippelt <dlippelt@f1r1s6.codam.nl>
2025-07-02 13:23:56 +0200

    Use better variable names - win_x -> screen_x - win_y -> screen_y

--- src/ft_init_mlx_setup_win.c
+++ src/ft_init_mlx_setup_win.c
@@ -6,7 +6,7 @@
 /*   By: dlippelt <dlippelt@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/17 17:15:13 by tmitsuya          #+#    #+#             */
-/*   Updated: 2025/07/02 12:02:09 by dlippelt         ###   ########.fr       */
+/*   Updated: 2025/07/02 13:22:39 by dlippelt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,20 @@
 
 static void	ft_set_win_dimesions(t_cub3d *cub)
 {
-	int	win_x;
-	int	win_y;
+	int	screen_x;
+	int	screen_y;
 
 	cub->win_x = WIN_X_DEFAULT;
 	cub->win_y = WIN_Y_DEFAULT;
-	mlx_get_screen_size(cub->mlx, &win_x, &win_y);
-	if ((win_y * 19/20) < cub->win_y)
+	mlx_get_screen_size(cub->mlx, &screen_x, &screen_y);
+	if ((screen_y * 19/20) < WIN_Y_DEFAULT)
 	{
-		cub->win_y = win_y * 19/20;
+		cub->win_y = screen_y * 19/20;
 		cub->win_x = cub->win_y * 4/3;
 	}
-	else if (win_x < cub->win_x)
+	else if (screen_x < WIN_X_DEFAULT)
 	{
-		cub->win_x = win_x;
+		cub->win_x = screen_x;
 		cub->win_y = cub->win_x * 3/4;
 	}
 }
@@ -38,9 +38,6 @@ int	ft_init_mlx_setup_win(t_cub3d *cub)
 	if (!cub->mlx)
 		return (ft_error(EMLXINIT));
 	ft_set_win_dimesions(cub);
-	// mlx_get_screen_size(cub->mlx, &cub->win_x, &cub->win_y);
-	// cub->win_x /= 2;
-	// cub->win_y = cub->win_y * 3 / 4;
 	cub->win = mlx_new_window(cub->mlx, cub->win_x, cub->win_y, TITLE);
 	if (!cub->win)
 		return (ft_error(EMLXWIN));
