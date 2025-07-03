d29e762bd27e9100a9c5a4e0f5942c3e6f7d5abf
Dominique Lippelt <dlippelt@f1r1s12.codam.nl>
2025-07-03 16:25:31 +0200

    Remove inclusion of rendering.h and instead add ft_set_img_addr() prototype to minimap.h as well - Note: This means ft_set_img_addr() is forward declared in both rendering.h AND minimap.h - There might be a better solution to this by using more header files when organizing the remaining files in the src folder

--- inc/minimap.h
+++ inc/minimap.h
@@ -6,7 +6,7 @@
 /*   By: dlippelt <dlippelt@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/30 10:35:20 by dlippelt          #+#    #+#             */
-/*   Updated: 2025/07/02 15:03:18 by dlippelt         ###   ########.fr       */
+/*   Updated: 2025/07/03 16:21:01 by dlippelt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,15 @@
 # define MINIMAP_H
 
 # include <stdio.h>
-# include "structs.h"
 # include "mlx.h"
 # include "utils.h"
-# include "rendering.h"
 # include "macros.h"
+# include "structs.h"
 
 int		ft_init_minimap(t_cub3d *cub);
 void	ft_draw_minimap(t_mmap *mmap);
 int		ft_get_cell_id(int line, int i, t_mmap *mmap);
 void	ft_add_objects(t_mmap *mmap);
+void	ft_set_img_addr(t_imgs *imgs);
 
 #endif
