9a8d0e2021168189af533de9acf9595bb5923bf5
Dominique Lippelt <dlippelt@f1r1s6.codam.nl>
2025-07-10 10:38:36 +0200

    Use new door texture

--- inc/macros.h
+++ inc/macros.h
@@ -6,7 +6,7 @@
 /*   By: dlippelt <dlippelt@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/16 15:08:02 by dlippelt          #+#    #+#             */
-/*   Updated: 2025/07/09 18:06:11 by dlippelt         ###   ########.fr       */
+/*   Updated: 2025/07/10 10:37:06 by dlippelt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@
 # define P_SPRITE_1 "textures/sprite_x256_1.xpm"
 # define P_SPRITE_2 "textures/sprite_x256_2.xpm"
 # define P_SPRITE_3 "textures/sprite_x256_3.xpm"
-# define P_DOOR "textures/gate_x256.xpm"
+# define P_DOOR "textures/plate_door_256.xpm"
 # define SP_SIZE_DOWN 2
 # define EPSILON 1e-6
 # define ANIMATION_SPEED_MILISEC 500
