86e69a20ab10c26b1e80c66c8de579621f2096b4
dlippelt <dlippelt@student.codam.nl>
2025-07-11 08:56:17 +0200

    Clearer if statement

--- src/validation/ft_get_element_info.c
+++ src/validation/ft_get_element_info.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_get_element_info.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tmitsuya <tmitsuya@student.codam.nl>       +#+  +:+       +#+        */
+/*   By: dlippelt <dlippelt@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/18 13:23:36 by dlippelt          #+#    #+#             */
-/*   Updated: 2025/06/25 17:08:29 by tmitsuya         ###   ########.fr       */
+/*   Updated: 2025/07/11 08:52:08 by dlippelt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,14 +23,14 @@ static void	ft_move_to_info(char **el)
 int	ft_get_element_info(t_file *f, char *el, int el_id)
 {
 	ft_move_to_info(&el);
-	if (el_id != COLFLOOR && el_id != COLCEILING)
+	if (el_id == COLFLOOR || el_id == COLCEILING)
 	{
-		if (!ft_new_tex(&f->tex, el, el_id))
+		if (!ft_new_col(&f->col, el, el_id))
 			return (0);
 	}
 	else
 	{
-		if (!ft_new_col(&f->col, el, el_id))
+		if (!ft_new_tex(&f->tex, el, el_id))
 			return (0);
 	}
 	return (1);
